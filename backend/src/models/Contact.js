f605504c89edb40646ba244eb1c80332cf73a9a8
aabboodi <155229950+aabboodi@users.noreply.github.com>
2025-11-12 03:35:20 +0200

    Create Contact model with schema definition

---
+++ backend/src/models/Contact.js
@@ -0,0 +1,24 @@
+// backend/src/models/Contact.js
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isFollower: {
+    type: Boolean,
+    default: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Contact', contactSchema);
